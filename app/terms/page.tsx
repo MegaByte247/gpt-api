72b7c5281512abc23edca82b4d16f17a363b1940
MegaByte247 <hmeg521@gmail.com>
2026-01-23 12:21:10 +0800

    Create page.tsx

---
+++ app/terms/page.tsx
@@ -0,0 +1,13 @@
+export default function TermsPage() {
+  return (
+    <main style={{ padding: 40 }}>
+      <h1>Terms of Service</h1>
+      <p>
+        This service is provided as-is with no guarantees.
+      </p>
+      <p>
+        Do not submit sensitive or personal information.
+      </p>
+    </main>
+  )
+}
